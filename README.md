af850aee60a84df73c93bdacc7f8d0a00d3cb317
Scott Limmer <scottlimmer@gmail.com>
2023-11-06 07:35:48 +1100

    initial site and theme scaffolding

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # scottlimmer.github.io
+
+## Init project (from WSL)
+docker run --rm --volume=".:/srv/jekyll:Z" -it jekyll/jekyll jekyll new --skip-bundle --force .
+
+## Run bundle
+docker run --rm --volume=".:/srv/jekyll:Z" -it jekyll/jekyll bundle install
---
+++ hugo.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+HUGO_SERVER_PORT=80
+HUGO_DOCKER_IMAGE=hugomods/hugo:exts
+SITE_ROOT="$(pwd)/docs"
+
+
+docker_run() {
+  docker run -v "$SITE_ROOT":/src --rm -p $HUGO_SERVER_PORT:1313 "$HUGO_DOCKER_IMAGE" "$@"
+}
+
+case "$1" in
+  npm)
+    docker_run "$@"
+    ;;
+  serve)
+    shift
+    docker_run hugo --environment local server --bind 0.0.0.0 "$@"
+    ;;
+  publish)
+    shift
+    docker_run hugo --environment local "$@"
+    ;;
+  *)
+    docker_run hugo --environment local  "$@"
+    ;;
+esac
